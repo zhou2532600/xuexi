fe2844660f11739b70a67361d52b8c046991d0d7
zhou2532600 <779804970@qq.com>
2020-01-18 19:26:16 +0800

    提交到本地了

---
+++ 容器/Course.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 容器
+{
+  public                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               class Course
+    { 
+         public int CourseId { get; set; }
+        public string Name { get => name; set => name = value; }
+        
+
+        private string name;
+        private double _price;
+        public double Price
+        {
+            get { return Price; }
+            set { Price = value; }
+
+
+        }
+       
+
+
+    }
+}
